1d7fee6dc3c852bbf3cbfa8989247ac86d6717e7
ghackeling <gavin.hackeling@vibrantmedia.com>
2013-09-12 14:54:29 -0400

    lowercased corpora files

--- src/main/java/com/gavinmhackeling/mallet/SUTopicModeler.java
+++ src/main/java/com/gavinmhackeling/mallet/SUTopicModeler.java
@@ -22,14 +22,18 @@ import cc.mallet.types.InstanceList;
 /**
  * @author ghackeling
  *
+ * /media/Storage/workspace/topicmodeling/src/main/resources/full.corpus
+ * /media/Storage/workspace/topicmodeling/src/main/resources/su.model
+ * /media/Storage/workspace/topicmodeling/src/main/resources/topics
+ * /media/Storage/workspace/topicmodeling/src/main/resources/test.corpus
+ * /media/Storage/workspace/topicmodeling/src/main/resources/report.csv
+ *
  */
 public class SUTopicModeler 
 {
 	private static final int NUM_THREADS = 4;
 	private static final int NUM_TOPICS = 100;
 	private static final int NUM_ITERATIONS = 50;
-	private static final String TEST = "Homemade Enchilada Sauce Lynn s Kitchen Adventures I usually buy my enchilada sauce Yes I knew I should be making it but I had never found a recipe that I was really happy with I had tried several and they just weren t very good So I stuck to the canned stuff you can get at the grocery store I was recently talking to a friend of mine about this She lived in Mexico for a few years so she knows some about Mexican cooking I asked her how she made her enchilada sauce She told me the basics and then gave me an exact recipe I decided to give it a try This recipe was really good This was the best enchilada sauce that I have made It had great flavor I think it was even better than the canned sauce My husband thought it could have been spicier But he likes his enchiladas spicy You can always add more chili powder or chilies if you like it really spicy The kids and I thought it was really good just like it is I did change two things It called for green onions I did not have any so I used regular onions I thought they worked great so I will probably continue to make it this way I also pureed everything in the blender I wanted a very smooth sauce If you want it more chunky just mix the ingredients together and do not blend Enchiladas are a pretty frugal meal and homemade enchilada sauce is a great way to make enchiladas even more frugal 2 8 ounce cans tomato sauce 1 4 ounce can chopped green chilies undrained 1 2 cup onion chopped 2 teaspoons chili powder 1 teaspoon ground cumin 1 4 teaspoon dried oregano 1 clove garlic minced Combine all tomato sauce ingredients and place in a blender Puree ingredients Then place in a saucepan Heat over medium heat until heated through about 5 minutes Use as desired for enchiladas Get your free Quick Easy Breakfasts ebook Just subscribe for free email updates from Lynn s Kitchen Adventures Like this article Share it this homemade enchilada sauce recipe came from a friend of mine who spent several years in mexico.";
-	private static final String TEST2 = "lolpics Stun grenade ar funny pictures at lolpics.se. the best funny images on the internet funny photo images, funny videos, can has cheezburger ,roliga bilder, lol pics, lolpics";
 	private static InstanceList instances;
 	private static ParallelTopicModel model;
 	private static Alphabet dataAlphabet;
@@ -42,12 +46,18 @@ public class SUTopicModeler
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
 
-		createModel(args[0], args[1]);
-		predict(args[2], args[3]);
+		loadModel();
+		createModel(args[0], args[1], args[2]);
+//		predict(args[3], args[4]);
 
 		long duration = System.currentTimeMillis()-startTime;
 		System.out.println("Completed in " + duration + " milliseconds");
 
+	}
+
+	private static void loadModel() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private static void predict(String testSetFileName, String outReportFileName) throws IOException 
@@ -60,13 +70,14 @@ public class SUTopicModeler
 
 		for (int i=0; i<testing.size(); i++)
 		{
+			System.out.println("----------------------------------------------------------------\nTest Case: " + i);
 			double[] testProbabilities = inferencer.getSampledDistribution(testing.get(i), 10, 1, 5);
 			for (int j=0; j<testProbabilities.length; j++)
 			{
 				fileWriter.append(testProbabilities[j] + ",");
 				if (testProbabilities[j] > 0.2)
 				{
-					System.out.println(j + ": " + testProbabilities[j]);
+					System.out.println("Topic: " + j + ": " + testProbabilities[j]);
 					Iterator<IDSorter> iterator = topicSortedWords.get(j).iterator();
 					int rank = 0;
 					while (iterator.hasNext() && rank < 10) {
@@ -76,13 +87,14 @@ public class SUTopicModeler
 					}
 				}
 			}
+			System.out.println("\n----------------------------------------------------------------\n");
 			fileWriter.append('\n');
 		}
 		fileWriter.flush();
 		fileWriter.close();		
 	}
 
-	private static void createModel(String trainingFileName, String outModelName) throws IOException 
+	private static void createModel(String trainingFileName, String outModelName, String outTopicsListName) throws IOException 
 	{
 		ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
 		pipeList.add(new CharSequence2TokenSequence(Pattern.compile("[\\p{L}\\p{N}_]+")));
@@ -110,17 +122,23 @@ public class SUTopicModeler
 		// Get an array of sorted sets of word ID/count pairs
 		topicSortedWords = model.getSortedWords();
 
+		FileWriter fileWriter = new FileWriter(new File(outTopicsListName));
 		for (int i=0; i<topicSortedWords.size(); i++)
 		{
 			Iterator<IDSorter> iterator = topicSortedWords.get(i).iterator();
 			int rank = 0;
 			System.out.print("Topic: " + i + ": ");
-			while (iterator.hasNext() && rank < 5) {
+			fileWriter.write("Topic: " + i);
+			while (iterator.hasNext() && rank < 12) {
 				IDSorter idCountPair = iterator.next();
 				System.out.print(dataAlphabet.lookupObject(idCountPair.getID()) + " ");
+				fileWriter.write(dataAlphabet.lookupObject(idCountPair.getID()) + " ");
 				rank++;
 			}
+			fileWriter.write('\n');
 			System.out.println();
 		}
+		fileWriter.flush();
+		fileWriter.close();
 	}
 }
